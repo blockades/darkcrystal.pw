ba663a39cd96ad14ef76efb53a85a36e98d2320e
Dan Hassan <dan@dyne.org>
2018-08-29 14:35:36 +1000

    adding todo

---
+++ darkcrystal/site/accounts/dh.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: dh
+password: >
+  $2y$10$HWu9zZjwAewCn1/6uWyhRuVd1p7WE5jbNZY8FMxc1dMGx2BVc/h8e
+email: dh@blockades.org
+language: en
+role: admin
+history:
+  - scuttlebutt-protocol-guide
+  - home
+  - dark-crystal-repos
