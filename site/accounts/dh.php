8e91ed35ee2325f2b39e8380f32063720249baf6
Dan Hassan <dan@dyne.org>
2018-08-31 16:05:15 +1000

    lifting all content to top directory

---
+++ site/accounts/dh.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: dh
+password: >
+  $2y$10$/SLFn3ZbS3WL73qsf1ubkeRKy8oeFXwcI3prHepVDGVK1D1iGi8IC
+email: dh@blockades.org
+language: en
+role: admin
+history:
+  - home
---
+++ site/templates/default.php
@@ -0,0 +1,33 @@
+<?php snippet('header') ?>
+
+<div class="fullscreen">
+  <?= $page->landing()->kirbytext() ?>
+</div>
+<main>
+
+<div class="intro">
+  <?= $page->intro()->kirbytext() ?>
+</div>
+
+<div class="secondary">
+  <?= $page->secondary()->kirbytext() ?>
+</div>
+</main>
+
+<div class="faq">
+  <div class="faq_title">
+    <?= $page->faq_title()->kirbytext() ?>
+  </div>
+
+  <div class="inner">
+    <div class="faq_left">
+      <?= $page->faq_left()->kirbytext() ?>
+    </div>
+
+    <div class="faq_right">
+      <?= $page->faq_right()->kirbytext() ?>
+    </div>
+  </div>
+</div>
+
+<?php snippet('footer') ?>
