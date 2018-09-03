68fe21ee452967409d954e9c6923eda65934134f
Dan Hassan <dan@dyne.org>
2018-09-03 22:20:42 +1000

    fixing missing template bug

---
+++ site/templates/default.php
@@ -0,0 +1,21 @@
+<nav><a href="http://darkcrystal.pw/">Dark Crystal</a> <a href="http://darkcrystal.pw/ssb-protocol-docs.html" target="_blank">Scuttlebutt Protocol Guide</a> <a href="https://github.com/blockades/" target="_blank">Dark Crystal Repos</a> <a href="https://github.com/ssbc" target="_blank">SSB Repos</a></nav><a class="email" href="mailto: frontdesk@blockades.org">Get in touch</a>
+
+<?= css('assets/css/index.css') ?>
+
+  <div class="main" role="main">
+
+    <header class="wrap">
+      <h1><?= $page->title()->html() ?></h1>
+      <div class="intro text">
+        <?= $page->intro()->kirbytext() ?>
+      </div>
+      <hr />
+    </header>
+
+    <div class="text wrap">
+      <?= $page->text()->kirbytext() ?>
+    </div>
+
+  </div>
+
+<?php snippet('footer') ?>
