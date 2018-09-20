de28e1fc36d71df369ab66e71640922166d2aeee
Dan Hassan <dan@dyne.org>
2018-09-20 14:05:08 +1000

    adding tiny hermes ico

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -6,7 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="assets/css/style.css">
   <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono" rel="stylesheet">
-
+  <link rel="shortcut icon" href="assets/images/hermes-tiny.ico" type="image/x-icon">
+  <link rel="icon" href="assets/images/hermes-tiny.ico" type="image/x-icon">
   <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
 
 </head>
