7441e37bcba6555747dc8cef3b93b14b783fe891
Dan Hassan <dan@dyne.org>
2018-10-19 11:49:05 +1100

    adding link to demo gif

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -8,9 +8,11 @@
   <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono" rel="stylesheet">
   <link rel="shortcut icon" href="assets/images/hermes-tiny.ico" type="image/x-icon">
   <meta property="og:image" content="assets/images/darkcrystal-logo.jpg">
+  <meta property="og:title" content="Dark Crystal">
+  <meta property="twitter:title" content="Dark Crystal">
   <link rel="icon" href="assets/images/hermes-tiny.ico" type="image/x-icon">
   <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
 
 </head>
 <body>
-<header><nav><a href="<?php echo $site->url() ?>/">dark crystal</a> <a href="<?php echo $site->url() ?>/about">about</a> <a href="<?php echo $site->url() ?>/contact">contact&repos</a> </header>
+<header><nav><a href="<?php echo $site->url() ?>/">dark crystal</a> <a href="<?php echo $site->url() ?>/about">about</a> <a href="./img/screencast.gif">demo</a> <a href="<?php echo $site->url() ?>/contact">contact&repos</a> </header>
