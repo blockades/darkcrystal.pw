befb58cc56d107293e15e89915bc8d2b85579a60
Dan Hassan <dh@blockades.org>
2019-03-10 13:35:28 +1100

    adding link to demo

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -15,4 +15,4 @@
 
 </head>
 <body>
-<header><nav><a href="<?php echo $site->url() ?>/">dark crystal</a> <a href="<?php echo $site->url() ?>/about">about</a> <a href="./img/screencast.gif">demo</a> <a href="<?php echo $site->url() ?>/contact">contact&repos</a> </header>
+<header><nav><a href="<?php echo $site->url() ?>/">dark crystal</a> <a href="<?php echo $site->url() ?>/about">about</a> <a href="./img/screencast.gif">demo</a> <a href="<?php echo $site->url() ?>/contact">contact</a> </header>
